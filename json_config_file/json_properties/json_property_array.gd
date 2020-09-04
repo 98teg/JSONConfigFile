bac4c1e6af3c8a102ebcff22ce1ac86bd2d3a706
98teg <teg98tom@gmail.com>
2020-09-04 10:21:07 +0200

    Remove the dependency of the array property with the comparator class

--- json_config_file/json_properties/json_property_array.gd
+++ json_config_file/json_properties/json_property_array.gd
@@ -75,7 +75,7 @@ func _validate_type(array) -> void:
 						var value_1 = get_result()[i]
 						var value_2 = get_result()[j]
 
-						if Comparator.are_equal(value_1, value_2):
+						if _are_equal(value_1, value_2):
 							_errors.append({
 								"error": Errors.ARRAY_TWO_ELEMENTS_ARE_EQUAL,
 								"element_1": i,
@@ -86,3 +86,42 @@ func _validate_type(array) -> void:
 			"error": Errors.WRONG_TYPE,
 			"expected": Types.ARRAY,
 		})
+
+static func _are_equal(value_1, value_2) -> bool:
+	if typeof(value_1) != typeof(value_2):
+		return false
+
+	if typeof(value_1) == TYPE_REAL:
+		return _are_equal_floats(value_1, value_2)
+	elif typeof(value_1) == TYPE_ARRAY:
+		return _are_equal_arrays(value_1, value_2)
+	elif typeof(value_1) == TYPE_DICTIONARY:
+		return _are_equal_dictionaries(value_1, value_2)
+	else:
+		return value_1 == value_2
+
+static func _are_equal_floats(float_1 : float, float_2 : float) -> bool:
+	return abs(float_1 - float_2) < JSONProperty.PRECISION_ERROR
+
+static func _are_equal_arrays(array_1 : Array, array_2 : Array) -> bool:
+	if array_1.size() != array_2.size():
+		return false
+
+	for i in array_1.size():
+		if not _are_equal(array_1[i], array_2[i]):
+			return false
+	
+	return true
+
+static func _are_equal_dictionaries(dic_1 : Dictionary, dic_2 : Dictionary) -> bool:
+	if dic_1.keys().size() != dic_2.keys().size():
+		return false
+
+	for key in dic_1.keys():
+		if dic_2.has(key):
+			if not _are_equal(dic_1[key], dic_2[key]):
+				return false
+		else:
+			return false
+	
+	return true
